3f6b33644dce64654617d0c92ac30eed9f5972ee
Mikael Isaksson <creftan@gmail.com>
2014-02-09 14:25:34 +0100

    no message

---
+++ config.lua
@@ -0,0 +1,37 @@
+
+application = {
+	content = {        --Small     Big
+		width = 320,   --160       320
+		height = 480,  --240       480
+		scale = "letterBox",
+		fps = 60,
+		
+		--[[
+        imageSuffix = {
+		    ["@2x"] = 2,
+		}
+		--]]
+	},
+
+    --[[
+    -- Push notifications
+
+    notification =
+    {
+        iphone =
+        {
+            types =
+            {
+                "badge", "sound", "alert", "newsstand"
+            }
+        }
+    }
+    --]]
+
+    LevelHelperSettings = 
+    {
+        imagesSubfolder = "Images",
+        levelsSubfolder = "levels",
+        directorGroup = nil
+    }
+}
