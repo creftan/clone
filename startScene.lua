4cda039e44e9c522db7af0382b70b87eea3043a0
Mikael Isaksson <creftan@gmail.com>
2014-02-10 16:54:19 +0100

    Startmenu

--- startScene.lua
+++ startScene.lua
@@ -35,18 +35,159 @@ function scene:createScene(e)
 ]]
 
 	local sceneView = self.view
+	
 	--[[	  VARIABLES		]]--
 
 	local menuTransition
 	local menuTransition2
 	local buttonGroup
 	local menuDown = false
+
+
+	--[[	BACKGROUND 		]]--
+
+
+	display.setDefault( "magTextureFilter", "nearest" )
+	display.setDefault( "minTextureFilter", "nearest" )
+
+	local p = {}
+	local parallax2 = display.newGroup()
+	local parallax1 = display.newGroup()
+	--local doge = display.newGroup()
+	sceneView:insert(parallax2)
+	sceneView:insert(parallax1)
+
+
+	function makescene()
+		--[[
+		doge_body = display.newImageRect(doge,"art/startScreen/doge.png",32,32)
+		doge_tail = display.newImageRect(doge,"art/startscreen/doge_tail.png",32,32)
+		doge_body.xScale = 2
+		doge_body.yScale = 2
+		doge_tail.xScale = 2
+		doge_tail.yScale = 2
+
+		doge.x = _W * .25
+		doge.y = _H * .4
+		doge.isVisible = false
+		]]--
+		p.position1 = 0 
+		p.position2 = 0 
+		p.position3 = 0 
+
+		parallax2.layer1_1=display.newImageRect(parallax2,"art/startScreen/mountain.png", 64,128)
+		parallax2.layer1_1.xScale = 2
+		parallax2.layer1_1.yScale = 2
+		parallax2.layer1_1.x = _W/2
+		parallax2.layer1_1.y = _H*.64
+
+		parallax2.layer1_2=display.newImageRect(parallax2,"art/startScreen/mountain.png", 64,128)
+		parallax2.layer1_2.xScale = 2
+		parallax2.layer1_2.yScale = 2
+		parallax2.layer1_2.x = _W/2 + 512
+		parallax2.layer1_2.y = _H*.64
+
+		parallax1.layer1_1=display.newImageRect(parallax1,"art/startScreen/bushes.png", 256,64)
+		parallax1.layer1_1.xScale = 2
+		parallax1.layer1_1.yScale = 2
+		parallax1.layer1_1.x = _W/2
+		parallax1.layer1_1.y = _H*.74
+
+		parallax1.layer1_2=display.newImageRect(parallax1,"art/startScreen/bushes.png", 256,64)
+		parallax1.layer1_2.xScale = 2
+		parallax1.layer1_2.yScale = 2
+		parallax1.layer1_2.x = _W/2 + 512
+		parallax1.layer1_2.y = _H*.74
+
+		parallax1.layer2_1=display.newImageRect(parallax2,"art/startScreen/clouds.png", 256,64)
+		parallax1.layer2_1.xScale = 2
+		parallax1.layer2_1.yScale = 2
+		parallax1.layer2_1.x = _W/2
+		parallax1.layer2_1.y = _H*.02
+
+		parallax1.layer2_2=display.newImageRect(parallax2,"art/startScreen/clouds.png", 256,64)
+		parallax1.layer2_2.xScale = 2
+		parallax1.layer2_2.yScale = 2
+		parallax1.layer2_2.x = _W/2 + 512
+		parallax1.layer2_2.y = _H*.02
+
+
+		floorgroup = display.newGroup()
+		topgroup = display.newGroup()
+
+		local sky = display.newRect (0,0,_W,_H)
+			sky:setFillColor(.5,.6,.9)
+			sky.x = _W*.5
+			sky.y = _H*.4
+		
+		local ground_top_1 = display.newImageRect(topgroup,"art/startScreen/ground_top_2.png",701,64)
+			ground_top_1.xScale = .5
+			ground_top_1.yScale = .5
+			ground_top_1.x = _W*.5
+			ground_top_1.y = _H*.8
+
+		local ground_top_2 = display.newImageRect(topgroup,"art/startScreen/ground_top_2.png",701,64)
+			ground_top_2.xScale = .5
+			ground_top_2.yScale = .5
+			ground_top_2.x = _W*.5 + 350
+			ground_top_2.y = _H*.8
+			
+		local floorbottom = display.newRect(floorgroup,0,0,_W,_H)
+			floorbottom.x = _W/2
+			floorbottom.y = _H*1.3
+			floorbottom:setFillColor(.62,.37,.16)
+
+		floorgroup:insert(topgroup)
+		sceneView:insert(floorgroup)
+		sky:toBack()
+
+		floorgroup.y = _H*.1
+	end 
+
+	local c = 0 
+
+	function move(speed)
+
+		c = c + 1 
+
+		p.position1 = p.position1 - speed
+
+		if p.position1 < -350 then 
+			p.position1 = 0 
+		end
+
+		topgroup.x = p.position 
+
+		p.position2 = p.position2 - speed*.5
+		if p.position2 < -512 then 
+			p.position2 = 0 
+		end
+
+		p.position3 = p.position3 - speed*.33
+		if p.position3 < -512 then 
+			p.position3 = 0 
+		end
+
+		topgroup.x = p.position1
+		parallax1.x = p.position2 
+		parallax2.x = p.position3 
+		--doge.y =_H * .4 + math.sin(c/10)*5
+		--doge_tail.rotation = 0 + math.sin(c/3)*10
+	end
+
+	function dostuff()
+		move (1.5)
+	end 
+
+
+	makescene()
+	Runtime:addEventListener("enterFrame",dostuff)
+
 	--[[		MENU 		]]--
 	
 
 	local function startGame(event)
 		menuTransitionFunc("startGame")
-		
 	end
 
 	local function startOptions()
@@ -72,23 +213,24 @@ function scene:createScene(e)
 						}, 
 					}
 
-	local menuList					= {"START","OPTIONS","HELP"}
+	local menuList	= {"START","OPTIONS","HELP"}
 
-	local menuGroup 				= display.newGroup()
-	menuGroup.x, menuGroup.y		= _W*.5, -_H
-	menuGroup.alpha 				= .5
-	
-	local menuRect					= display.newRect(menuGroup,0,0,250,350)
+	local menuGroup = display.newGroup()
+	menuGroup.x, menuGroup.y = _W*.5, -_H
+	menuGroup.alpha = .5
+	menuGroup:toFront()
+
+	local menuRect = display.newRect(menuGroup,0,0,250,350)
 	
 	for i=1,#menuList do
-		buttonGroup					= display.newGroup()
-		buttonGroup.y 				= -180
+		buttonGroup	= display.newGroup()
+		buttonGroup.y = -180
 
-		local buttons				= display.newRect(buttonGroup,0,0,120,60)
-		buttons.y 					= buttons.height*i*1.5
+		local buttons = display.newRect(buttonGroup,0,0,120,60)
+		buttons.y = buttons.height*i*1.5
 		
-		local buttonText 			= display.newText(buttonGroup,menuFunc[i].Label,0,0,nil,14)
-		buttonText.x, buttonText.y 	= buttons.x, buttons.y
+		local buttonText = display.newText(buttonGroup,menuFunc[i].Label,0,0,nil,14)
+		buttonText.x, buttonText.y = buttons.x, buttons.y
 
 		menuGroup:insert(buttonGroup)
 
@@ -103,22 +245,31 @@ function scene:createScene(e)
 	--[[	   EFFECTS 		]]--
 
 
+
+	function transitionFunc(object,timeTrans,yPos,transitionType,completed)
+		local transition = transition.to(object,{time=timeTrans,y=yPos,transition=transitionType,onComplete=completed})
+		print("Here again")
+	end
+
 	function menuTransitionFunc(scene)
 		if not menuDown then
-			local menuTransition = transition.to(menuGroup, {time=100,y=_H*.55,transition=easing.InOutQuad,onComplete=function()
-					local menuTransition2 = transition.to(menuGroup,{time=100,y=_H*.5})
-			end})
+			transitionFunc(menuGroup,100,_H*.55,easing.InOutQuad,function()
+				transitionFunc(menuGroup,100,_H*.5,easing.InOutQuad)
+			end)
 			menuDown = true
 		else
-			local menuTransition = transition.to(menuGroup, {time=100,y=_H*.55,transition=easing.InOutQuad,onComplete=function()
-					local menuTransition2 = transition.to(menuGroup,{time=100,y=-_H,onComplete=function()
-						storyboard.gotoScene(scene)
-					end})
-			end})
+			transitionFunc(menuGroup,100,_H*.55,easing.InOutQuad,function()
+				transitionFunc(menuGroup,100,-_H,easing.InOutQuad,function()
+					storyboard.gotoScene(scene)
+				end)
+			end)
 		end
 	end
 	sceneView:insert(menuGroup)
 	menuTransitionFunc()
+
+
+
 end 
 
 function scene:enterScene(e)
@@ -143,10 +294,34 @@ display.remove(scorebutton)
 display.remove(buttonGroup)
 display.remove(backgroundGroup)
 
+display.remove(parallax2)
+display.remove(parallax1)
+display.remove(parallax2)
+display.remove(layer1_1)
+display.remove(parallax2)
+display.remove(layer1_2)
+display.remove(layer1_2)
+display.remove(parallax1)
+display.remove(layer1_1)
+display.remove(parallax1)
+display.remove(layer1_2)
+display.remove(parallax1)
+display.remove(layer2_1)
+display.remove(parallax1)
+display.remove(layer2_2)
+display.remove(floorgroup)
+display.remove(topgroup)
+display.remove(sky)
+display.remove(ground_top_1)
+display.remove(ground_top_2)
+display.remove(floorbottom)
 
 menuTransition2 = nil
 menuTransition 	= nil
 
+Runtime:removeEventListener("enterFrame",dostuff)
+
+local sceneView = self.view
 
 end 
 
