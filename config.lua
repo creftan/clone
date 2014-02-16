fcbc336ce4576acb968b01e1735d956f02106c9c
Mikael Isaksson <creftan@gmail.com>
2014-02-16 17:25:50 +0100

    GooglePlay Key added.

--- config.lua
+++ config.lua
@@ -1,5 +1,14 @@
 
 application = {
+       license =
+       {
+          google =
+          {
+             key = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAjb6VZ2ofuNe0GlOiFnLJKkT/PSuNn4LPIwtW+DqAKY0Gp1jprQ/52A5YxMh4T6UYH5ZFOD3qB50QkIoOyuVVRw5k6b5FF+z+WfdFPc/1f6poWy4buYHtXRYOBI6HbYEkDp+I0pOGoMhjhmGw6u54SYP0zWv1EUdxLHDVqCXhx6tB5xci8/AE7fyUrXOcp0EuGYK4oqAwsqrD11Bkxvnkd2afMGSV3+jS7YwsSOvZpnusLd8p7Xafzpak7n1jWf4RGmxa/BsRIMNzsPCSRKaWmRgZt2yFy4TeIMzPAF7nHR/8eSduPyM94Z5Aho0SxNMojNS6PNjYkBaAaO3Qg+kMxwIDAQAB",
+             policy = "serverManaged",
+          },
+       },
+       
 	content = {        --Small     Big
 		width = 320,   --160       320
 		height = 480,  --240       480
@@ -35,3 +44,6 @@ application = {
         directorGroup = nil
     }
 }
+
+
+
